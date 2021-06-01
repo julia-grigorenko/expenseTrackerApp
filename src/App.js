990fb32a1c95a5ef530748b5ad61907d3722a45b
Julia Grigorenko <jvgrigorenko@gmail.com>
2021-06-01 16:53:37 +0200

    add AddTransaction component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
 import { GlobalProvider } from './context/GlobalState';
 
@@ -16,6 +17,7 @@ function App() {
         <Balance />
         <IncomeExpenses />
         <TransactionList />
+        <AddTransaction />
       </div>
     </GlobalProvider>
   );
