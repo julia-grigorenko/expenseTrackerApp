2b103ab38e849963625402e8075bbd6cd0bbb30e
Julia Grigorenko <jvgrigorenko@gmail.com>
2021-06-01 16:47:12 +0200

    add Transaction and TransactionList components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+
+import { GlobalProvider } from './context/GlobalState';
 
 import './App.css';
 
@@ -12,6 +15,7 @@ function App() {
       <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
       </div>
     </GlobalProvider>
   );
